4fbfac38eb42ea5510da43d91783e3e7845bce7d
idsherma <idsherma@gmail.com>
2020-08-26 23:10:28 -0400

    backend successfully setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const chalk = require('chalk');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 //chalk colors
 const error = chalk.bold.red;
@@ -20,6 +23,15 @@ const db = require("./config/keys").mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log(success("MongoDB successfully connected!")))
   .catch(err => console.log(error(`ERROR!:`, err)));
+
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport')(passport);
+
+//routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(msg(`Server up and running on port: ${port}!`)));
